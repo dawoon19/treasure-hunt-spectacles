524e95d7376750ebd69961b48f6abc1e27ad5017
Daniel He <tcdanielh@berkeley.edu>
2024-10-20 02:59:52 -0700

    try fix stuffs

--- Spectacles/Assets/TreasureChestAssets/TreasureChestAnimation.ts
+++ Spectacles/Assets/TreasureChestAssets/TreasureChestAnimation.ts
@@ -16,13 +16,7 @@ export class SpawnTreasureAnimation extends BaseScriptComponent {
     createObjectsFromPrefab( trailEffectPrefab:ObjectPrefab, chestPrefab:ObjectPrefab, sparklesPrefab:ObjectPrefab) {
         if (trailEffectPrefab && chestPrefab) {
             var trailEffect = trailEffectPrefab.instantiate(this.getSceneObject());
-<<<<<<< HEAD
             var chest = chestPrefab.instantiate(this.getSceneObject());
-=======
-            //trailEffect.getTransform().setWorldPosition(new vec3(0,0,-50));
-            var chest = chestPrefab.instantiate(this.getSceneObject());
-            //chest.getTransform().setWorldPosition(new vec3(0,0,-50));
->>>>>>> 91ac87dbe491de799df2c304a1d57d641e7a9a4e
             var sparkles = sparklesPrefab.instantiate(this.getSceneObject());
             print("we did it.")
         }
